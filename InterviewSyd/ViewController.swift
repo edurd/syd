c82657fbe0222603290bce68c242e63d9b7bb171
Eduardo Hoyos <edu.hoyos92@gmail.com>
2024-10-22 12:10:59 -0500

    Initial Commit

---
+++ InterviewSyd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterviewSyd
+//
+//  Created by Eduardo Hoyos on 22/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
